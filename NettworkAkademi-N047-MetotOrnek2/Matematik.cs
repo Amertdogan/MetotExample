4d6c3f592f1b502d696749206c7cdd3335a233e0
Ali Mert DOĞAN <alimertdoganytu@gmail.com>
2021-11-23 17:33:39 +0300

    Add project files.

---
+++ NettworkAkademi-N047-MetotOrnek2/Matematik.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NettworkAkademi_N047_MetotOrnek2
+{
+    class Matematik
+    {
+        //Toplama
+       public decimal toplamaIslemi(decimal sayi1,decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        //Çıkartma
+        public decimal cikartmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        //Çarpma
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        //Bölme
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        public void menuHazirla()
+        {
+            Console.WriteLine("*********MENU*********");
+            Console.WriteLine("1-TOPLAMA");
+            Console.WriteLine("2-ÇIKARTMA");
+            Console.WriteLine("3-ÇARPMA");
+            Console.WriteLine("4-BÖLME");
+            Console.Write("Lütfen yaptırmak istediginiz islemi seciniz.");
+        }
+
+        public void sonucEkranaYaz(decimal kullanıcıSay1,decimal kullanıcıSayi2,decimal sonuc,string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}",kullanıcıSay1,operators,kullanıcıSayi2,sonuc);
+        }
+
+
+
+    }
+}
---
+++ NettworkAkademi-N047-MetotOrnek2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NettworkAkademi_N047_MetotOrnek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        #region
+        /*Matematik adında bir class oluşturun.
+         * Bir menü hazırlayın metot ile
+         * Menüde
+         * "*****Menü*****"
+         * "1-Toplama"
+         * "2-Çıkartma"
+         * "3-Çarpma"
+         * "4-Bölme"
+         * "Lütfen bir deger seçiniz"
+         * Metot ile 4 işlemleri ekrana  yazdırılacak.
+         * public static void içinde matematik sınıfının bir instance'ini al
+         * kullanıcıdan 2 deger iste ve Matematik sınıfındaki 4 işlem metotunu kullanarak
+         * sonucları ekrana yazdır.switch case
+         * Yeniden iişlem yapmak isteyip istemediğini sorunuz.
+         */
+
+        #endregion
+        baslangıc:
+            
+
+            int secim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bir sayı giriniz.");
+            int sayı1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bir sayı daha giriniz.");
+            int sayı2 = Convert.ToInt32(Console.ReadLine());
+            switch (secim)
+
+            {
+                case 1:
+                    int toplama = sayı1 + sayı2;
+                    Console.WriteLine("Toplama sonucu " + toplama);
+                    break;
+                case 2:
+                    int cikartma = sayı1 - sayı2;
+                    Console.WriteLine("Çıkartma sonucu " + cikartma);
+                    break;
+                case 3:
+                    int carpma = sayı1 * sayı2;
+                    Console.WriteLine("Çarpma sonucu " + carpma);
+                    break;
+                case 4:
+                    int bolme = sayı1 / sayı2;
+                    Console.WriteLine("Bölme sonucu " + bolme);
+                    break;
+
+
+            }
+            Console.WriteLine("Tekrardan islem yapmak istiyor musunuz ? E/H");
+            String secim2 = Console.ReadLine();
+            if (secim2.ToUpper() == "E")
+            {
+                goto baslangıc;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
